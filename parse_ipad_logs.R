0c2af977a7273e512013edd944af48f490073d5e
Laura Leppänen <laura.leppanen@iki.fi>
2011-09-21 16:19:24 +0300

    Log timestamps as well, add some instructions.

--- parse_ipad_logs.R
+++ parse_ipad_logs.R
@@ -1,8 +1,37 @@
 # Parse iPad multitouch logs into matrices usable by the infocapacity
-# script. See the comments on the parse_logs function for instructions
-# for use.
+# script. Requires R version >= 2.11.
 #
-# by Laura Leppänen, 15th September 2011
+# Load the file in R and call the parse_logs(). The result files are
+# saved into a subdirectory which is by default called "data". The
+# current directory is assumed to contain the log files (named
+# <number>.log).
+#
+# The parse_logs function also optionally takes two parameters: logdir
+# which is the directory containing the logs and destination which is the
+# directory the results will be written to.
+#
+# Note that parsing ipad logs reliably is practically impossible in
+# some cases due to the lack of identifiers on touch points. Therefore
+# the results should always be checked and if necessary, corrected by
+# hand. (You can also change the guess_correct_features function slightly
+# to look at Y coordinates instead of X coordinates if they seem like
+# better indicators in your data. Brief instructions included in the function
+# comments.)
+#
+# Also note that parsing for more than two simultaneous touch points is
+# not implemented in the current version due to lack of test data. (Feel
+# free to add this functionality if required.)
+# 
+# ======
+# TL;DR:
+# Usage: parse_logs() or
+# parse_logs("ipad_logfile_directory", "result_directory")
+# Check results before further use.
+# Does not work with data that has more than two simultaneous touch
+# points. Support for more touch points should be implemented.
+# ======
+#
+# by Laura Leppänen, 21st September 2011
 
 library(stringr)
 # Note: stringr requires R version 2.11
@@ -32,7 +61,7 @@ find_prev <- function(curr, col) {
 # Returns the index of the next full line.
 find_next <- function(max_tps, curr) {
     nxt = curr + 1
-    while (length(veclist[[nxt]])/2 != max_tps)
+    while ((length(veclist[[nxt]]) - 1)/2 != max_tps)
         nxt = nxt + 1
     return(nxt)
 }
@@ -45,23 +74,30 @@ find_next <- function(max_tps, curr) {
 parse_values_to_veclist <- function(lines) {
     max_tps <- 0
     skip = c(FALSE, FALSE, TRUE)
+    last_timestamp = list(0.0)
 
     for (j in 1:length(lines)) {
-        skipi <- c()
-        line <- strsplit(lines[j], "//")[[1]][1]
-        if (is.na(line)) {
+        skipi <- c(FALSE) # the first value (ms) will never be skipped
+        line <- strsplit(lines[j], "//")[[1]]
+        if (is.na(line[1])) {
             print("Encountered empty line in the log file.")
             next
         }
 
-        veclist[[j]] <<- lapply(str_extract_all(line, "[0-9]+")[[1]], as.numeric)
+        time <- list(as.numeric(str_extract(line[2], "[0-9]+\\.[0-9]+")))
+        pointdata <- lapply(str_extract_all(line[1], "[0-9]+")[[1]], as.numeric)
 
-        touchpoints <- length(veclist[[j]])/3
+        timestamp <- list(last_timestamp[[1]] + time[[1]])
+        veclist[[j]] <<- c(timestamp, pointdata)
+        last_timestamp <- timestamp
+
+        touchpoints <- (length(veclist[[j]]) - 1)/3
         if (touchpoints > max_tps)
             max_tps <- touchpoints
 
-        # Skip every third value in the vector (these are the "taps: 1" or "taps: 0"
-        # values that mostly stay constant) because they do not appear informative.
+        # Skip every third value in the vector/list (these are the "taps: 1" or
+        # "taps: 0" values that mostly stay constant) because they do not appear
+        # informative.
         for (k in 1:touchpoints)
             skipi <- c(skipi, skip)
         veclist[[j]] <<- veclist[[j]][!skipi]
@@ -85,19 +121,22 @@ guess_correct_features <- function(curr, other1, other2 = NULL) {
     if (is.null(other2))
         other2 <- other1
 
-    dist_to_otherx1 <- abs(veclist[[curr]][[1]] - veclist[[other1]][[1]])
-    dist_to_otherx2 <- abs(veclist[[curr]][[1]] - veclist[[other2]][[3]])
+    dist_to_otherx1 <- abs(veclist[[curr]][[2]] - veclist[[other1]][[2]])
+    dist_to_otherx2 <- abs(veclist[[curr]][[2]] - veclist[[other2]][[4]])
 
-    dist_to_othery1 <- abs(veclist[[curr]][[2]] - veclist[[other1]][[2]])
-    dist_to_othery2 <- abs(veclist[[curr]][[2]] - veclist[[other2]][[4]])
+    # Y coordinates could be used instead of X coordinates if they produce
+    # better results in some circumstances. Remember to change the if
+    # statement below.
+    #dist_to_othery1 <- abs(veclist[[curr]][[3]] - veclist[[other1]][[3]])
+    #dist_to_othery2 <- abs(veclist[[curr]][[3]] - veclist[[other2]][[5]])
     
-    # Choose the X and Y feature pair with values closest to the values on
-    # the current row. A word of caution: this might not work so well
-    # with very fast touch point movements.
-    if (dist_to_otherx1 < dist_to_otherx2 && dist_to_othery1 < dist_to_othery2) {
+    # Choose the X feature with values closest to the values on the current
+    # row. A word of caution: this might not work so wellwith very fast
+    # touch point movements.
+    if (dist_to_otherx1 < dist_to_otherx2) {
         return(c(veclist[[curr]], missing_val))
     } else
-        return(c(missing_val, veclist[[curr]]))
+        return(c(veclist[[curr]][1], missing_val, veclist[[curr]][2:3]))
 }
 
 # Fills in empty value pairs in the vector list (global variable veclist)
@@ -107,11 +146,11 @@ fill_in_empty_values <- function(max_tps) {
     max_seen = FALSE
 
     for (j in 1:length(veclist)) {
-        if (length(veclist[[j]])/2 == max_tps) # do nothing if no values missing
+        if ((length(veclist[[j]]) - 1)/2 == max_tps) # do nothing if no values missing
             next
 
-        prev1 <- find_prev(j, 1)
-        prev2 <- find_prev(j, 3)
+        prev1 <- find_prev(j, 2)
+        prev2 <- find_prev(j, 4)
 
         if (prev1 == 0) { # missing values are at the beginning of the file
             # need to find the next full row to find out which features the existing
