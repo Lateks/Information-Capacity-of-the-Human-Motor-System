f1841321e03e8a711efe85d4e5ec3d8e3abedf7e
Laura Leppänen <laura.leppanen@iki.fi>
2011-09-22 23:57:28 +0300

    Refactor all the things! #2
    More renaming.

--- infocapacity.R
+++ infocapacity.R
@@ -20,25 +20,28 @@
 #####################################################################
 
 options(width=Sys.getenv("COLUMNS"));
+
+use_warnings = FALSE;
 twopie = 2*pi*exp(1);
 
 # Return stochastic complexity of sequence r with side information v
-evalSC <- function(v, r, war = FALSE) {
-    n <- nrow(r); # number of frames
+evalSC <- function(v, r) {
+    frames <- nrow(r);
     feature_code_length <- array(0,ncol(r));
     total_code_length <- 0;
+    residual_sums = list();
     for (i in 1:ncol(r)) {
         # extract regressors depending on the input size
         if (ncol(v) == ncol(r)) {
-            Aplus <- qr(cbind(matrix(1,n,1), v[, i]));
+            Aplus <- qr(cbind(matrix(1,frames,1), v[, i]));
             k = 3;
         }
         if (ncol(v) == 2 * ncol(r)) {
-            Aplus <- qr(cbind(matrix(1,n,1), v[, i], v[, ncol(r)+i]));
+            Aplus <- qr(cbind(matrix(1,frames,1), v[, i], v[, ncol(r)+i]));
             k = 4;
         }
         if (ncol(v) == 3 * ncol(r)) {
-            Aplus <- qr(cbind(matrix(1,n,1), v[, i], v[, ncol(r)+i], 
+            Aplus <- qr(cbind(matrix(1,frames,1), v[, i], v[, ncol(r)+i], 
                         v[, 2*ncol(r)+i]));
             k = 5;
         }
@@ -53,50 +56,50 @@ evalSC <- function(v, r, war = FALSE) {
         res <- qr.resid(Aplus, r[, i]);
         
         # warning about possibly too low variance
-        if (war)
-            if (sum(res^2) < .01) print(c(i,sum(res^2)))
+        if (use_warnings)
+            if (sum(res^2) < .01) print(c(i,sum(res^2)));
 
-        rss <- sum(res^2)
+        residual_sum <- sum(res^2);
+        residual_sums[[i]] <- residual_sum;
 
         # Rissanen's classic two-part MDL code-length (feel free
         # to replace by more advanced universal code).
-        MDL <-  n/2*log(twopie*rss/n) + k/2*log(n);
+        MDL <-  frames/2*log(twopie*residual_sum/frames) + k/2*log(frames);
         
         feature_code_length[i] <- MDL;
         total_code_length <- total_code_length + MDL;
     }
-    return(list(totCL=total_code_length, featCL=feature_code_length, rss=rss));
+    return(list(total = total_code_length, feature = feature_code_length,
+        residual_sums = residual_sums));
 }
 
 # Return residuals from AR(2) as a list where each element is the
 # residual vector for the corresponding feature
-evalR <- function(v,r) {
-    n <- nrow(r); # number of frames
-    res = list();
+residuals <- function(v,r) {
+    frames <- nrow(r);
+    residuals = list();
     # extract regressors depending on the input size
     for (i in 1:ncol(r)) {
         if (ncol(v) == ncol(r)) {
-            Aplus <- qr(cbind(matrix(1,n,1), v[, i]));
+            Aplus <- qr(cbind(matrix(1,frames,1), v[, i]));
             k <- 3;
         }
         if (ncol(v) == 2 * ncol(r)) {
-            Aplus <- qr(cbind(matrix(1,n,1), v[, i], v[, ncol(r)+i]));
+            Aplus <- qr(cbind(matrix(1,frames,1), v[, i], v[, ncol(r)+i]));
             k <- 4;
         }
-        res[[i]] <- qr.resid(Aplus, r[, i]);  
+        residuals[[i]] <- qr.resid(Aplus, r[, i]);  
     }
-    return(res);
+    return(residuals);
 }
 
-# Shared information of sequences a and b using complexity
-# defined by residuals
-shared_information <- function(a,b) {
+shared_information_by_residuals <- function(a,b) {
     n <- min(nrow(a), nrow(b))-2;
     total_shared <- 0;
     feature_shared <- array(0,ncol(a));
     
-    resa <- evalR(cbind(a[1:n,],a[1:n+1,]), a[1:n+2,]);
-    resb <- evalR(cbind(b[1:n,],b[1:n+1,]), b[1:n+2,]);
+    resa <- residuals(cbind(a[1:n,],a[1:n+1,]), a[1:n+2,]);
+    resb <- residuals(cbind(b[1:n,],b[1:n+1,]), b[1:n+2,]);
 
     for (i in 1:length(resa)) {
         lm.ra = lm(resa[[i]] ~ resb[[i]]);
@@ -111,30 +114,32 @@ shared_information <- function(a,b) {
         total_shared <- total_shared + shared_information;
     }
 
-    return(list(n=n, total_shared=total_shared, feature_shared=feature_shared));
+    return(list(n = n, total_shared = total_shared, feature_shared = feature_shared));
 }
 
 # Complexity of a single (multivariate) sequence
-SC <- function(a, war=FALSE) {
+SC <- function(a) {
     n <- nrow(a)-2;
     if (n < 1)
         return(list(n = 0,k = 1,cl = 0,rate = 0));
     
-    SC <- evalSC(cbind(a[1:n,],a[1:n+1,]), a[1:n+2,], war=war);
-    cl <- SC$totCL;
-    fcl <- SC$featCL;
-    rss <- SC$rss;
-    return(list(n=n, cl=cl, rate=cl/n, fcl=fcl, rss=rss));
+    SC <- evalSC(cbind(a[1:n,],a[1:n+1,]), a[1:n+2,]);
+    complexity <- SC$total;
+    feature_complexity <- SC$feature;
+    residual_sums <- SC$residual_sums;
+    return(list(n = n, complexity = complexity, rate = complexity/n,
+        feature_complexity = feature_complexity, residual_sums = residual_sums));
 }
 
 # Complexity of a (multivariate) sequence given another
-SCcond <- function(a,b, war=FALSE) {
+SCcond <- function(a,b) {
     n <- min(nrow(a), nrow(b))-2;
-    SC <- evalSC(cbind(a[1:n,],a[1:n+1,],b[1:n+2,]), a[1:n+2,], war=war);
-    cl <- SC$totCL;
-    fcl <- SC$featCL;
-    rss <- SC$rss;
-    return(list(n=n, cl=cl, rate=cl/n, fcl=fcl, rss=rss));
+    SC <- evalSC(cbind(a[1:n,],a[1:n+1,],b[1:n+2,]), a[1:n+2,]);
+    complexity <- SC$total;
+    feature_complexity <- SC$feature;
+    residual_sums <- SC$residual_sums;
+    return(list(n = n, complexity = complexity, rate = complexity/n,
+        feature_complexity = feature_complexity, residual_sums = residual_sums));
 }
 
 # Removes duplicated rows from both sequences.
@@ -150,7 +155,6 @@ remove_duplicate_frames <- function(a, b) {
     return(list(a, b));
 }
 
-# Perform normalization on the features of a matrix.
 normalize_features <- function(a) {
     a <- t(apply(a,1,'-',apply(a,2,mean)));
     a <- t(apply(a,1,'/',sqrt(apply(a,2,var))));
@@ -159,67 +163,78 @@ normalize_features <- function(a) {
     return(a);
 }
 
-# Add noise on the features of a matrix.           
-add_noise <- function(a, x) {
-    nfeat <- ncol(a);
-    for (k in 1:nfeat) {
-        feat_var <- var(a[,k]);
-        a[,k] <- a[,k] + x * feat_var * rnorm(nrow(a));
+# Add noise to the features of a matrix.    
+add_noise_to_features <- function(a, x) {
+    features <- ncol(a);
+    for (k in 1:features) {
+        feature_var <- var(a[,k]);
+        a[,k] <- a[,k] + x * feature_var * rnorm(nrow(a));
     }
     return(a);
 }
 
 # Calculate throughput for a given pair of matrices.
-throughput <- function(a, b, fps = 120, pca = FALSE, res = FALSE, ftp = FALSE, war = FALSE) {
+throughput <- function(a, b, fps = 120, pca = FALSE, residuals = FALSE,
+    features = FALSE) {
+
     lena <- nrow(a);
 
     if (pca) {
         principal_components <- prcomp(a, retx = TRUE, center = TRUE, scale. = TRUE);
 
         sum <- 0;
-        number_of_eigenvectors <- 0;
+        eigenvectors <- 0;
         threshold <- 0.9*sum(principal_components$sdev**2);
         for (k in 1:length(principal_components$sdev)) {
             sum <- sum + principal_components$sdev[k]**2;
-            number_of_eigenvectors <- k;
+            eigenvectors <- k;
             if (sum >= threshold)
                 break;
         }
-        eigenvectors <- pcs$rotation[,1:number_of_eigenvectors];
+        eigenvectors <- pcs$rotation[,1:eigenvectors];
         a <- a %*% eigenvectors;
         b <- b %*% eigenvectors;
     }
     
-    if (res) { # Return shared information determined by residuals
-        shared_information <- shared_information(a, b);
+    if (residuals) {
+        shared_information <- shared_information_by_residuals(a, b);
         throughput <- shared_information$total_shared/lena*120/log(2.0);
-        feature_throughput <- shared_information$feature_shared/lena*120/log(2.0);
-        return(list(tp=throughput, featTP=feature_throughput));
+        feature_throughputs <- shared_information$feature_shared/lena*120/log(2.0);
+        return(list(throughput = throughput,
+            feature_throughputs = feature_throughputs));
     }
 
-    SC_a <- SC(a, war);
-    SC_a_b <- SCcond(a, b, war);
-    complexity_of_a <- SC_a$cl;
-    feature_complexity_of_a <- SC_a$fcl;
-    complexity_of_a_cond_b <- SC_a_b$cl;
-    feature_complexity_of_a_cond_b <- SC_a_b$cl;
+    SC_a <- SC(a);
+    SC_a_b <- SCcond(a, b);
+    complexity_of_a <- SC_a$complexity;
+    feature_complexity_of_a <- SC_a$feature_complexity;
+    complexity_of_a_cond_b <- SC_a_b$complexity;
+    feature_complexity_of_a_cond_b <- SC_a_b$feature_complexity;
 
     throughput <- (complexity_of_a - complexity_of_a_cond_b)/lena*fps/log(2.0);
     
-    if (ftp) {
+    if (features) {
         feature_throughputs <- array(0,ncol(a));
         for (k in 1:ncol(a)) {
             feature_throughputs[k] <- (feature_complexity_of_a[k] -
                 feature_complexity_of_a_cond_b[k])/lena*120/log(2.0);
         }
-        return(list(tp=throughput, featTP = feature_throughputs));
+        return(list(throughput = throughput,
+            feature_throughputs = feature_throughputs));
     }
 
-    return(list(tp=throughput));
+    return(list(throughput = throughput));
 }
 
 # Calculate throughput for a given pair of files.
-TPpair <- function(filename1, filename2, fps = 120, pca = FALSE, amc = FALSE, res = FALSE, ftp = FALSE, war = FALSE, noise = 0) {
+#
+# See the dir_throughput function for descriptions of optional parameters.
+pair_throughput <- function(filename1, filename2, fps = 120, pca = FALSE,
+    amc = FALSE, residuals = FALSE, features = FALSE, warnings = FALSE,
+    noise = 0) {
+    
+    use_warnings <<- warnings;
+
     a <- read.table(filename1);
     b <- read.table(filename2);
 
@@ -230,29 +245,25 @@ TPpair <- function(filename1, filename2, fps = 120, pca = FALSE, amc = FALSE, re
         b$V34=NULL; b$V46=NULL;
     }
 
-    datams <- remove_duplicate_frames(a, b);
-    a <- datams[[1]];
-    b <- datams[[2]];
+    data <- remove_duplicate_frames(a, b);
+    a <- data[[1]];
+    b <- data[[2]];
 
     if (noise > 0) {
-        a <- add_noise(a, noise);
-        b <- add_noise(b, noise);
+        a <- add_noise_to_features(a, noise);
+        b <- add_noise_to_features(b, noise);
     }
 
     a <- normalize_features(a);
     b <- normalize_features(b);
 
-
-    return(throughput(a, b, fps = fps, pca = pca, res = res, ftp = ftp, war = war));
+    use_warnings <<- FALSE;
+    return(throughput(a, b, fps = fps, pca = pca, residuals = residuals,
+        features = features));
 }
 
 # Calculate throughput for all aligned sequence pairs in the directory (by
-# default the current working directory). Boolean parameters for the use of
-# Principal Components analysis and AMC data (instead of coordinate data).
-# Currently sequences 1 and 2, 3 and 4, 5 and 6 and so on are assumed to
-# be repetitions of the same sequence.
-#
-# Framerate (fps) can be specified if it differs from 120.
+# default the current working directory).
 #
 # The function assumes that the original amc or coordinate files are in the
 # working directory. (Note: all files should be for the same test subject!)
@@ -262,7 +273,18 @@ TPpair <- function(filename1, filename2, fps = 120, pca = FALSE, amc = FALSE, re
 # in files that are named in the following way:
 #         <seq1 #>_ali_<seq2 #>.txt
 # Example: "14_ali_15.txt".
-TPdir <- function(fps = 120, pca = FALSE, amc = FALSE, res = FALSE, ftp = FALSE, war = FALSE, noise = 0) {
+#
+# Parameters:
+# - fps         frames per second
+# - pca         use principal components analysis
+# - amc         input data is in AMC format
+# - residuals   use residuals to determine complexities
+# - features    also calculate throughput for individual features
+# - warnings    print warnings of low residual variance (to debug NaN results)
+# - noise       ???
+dir_throughput <- function(fps = 120, pca = FALSE, amc = FALSE, residuals = FALSE,
+    features = FALSE, warnings = FALSE, noise = 0) {
+
     if (amc) {
         files <- dir(".", "^[[:digit:]]+.amc");
     } else
@@ -273,20 +295,29 @@ TPdir <- function(fps = 120, pca = FALSE, amc = FALSE, res = FALSE, ftp = FALSE,
     feature_throughputs <- array(list(NULL), c(sequences/2, 2));
 	
     for (i in 1:(sequences/2)) {
-        j = 2*i;
-        k = j-1;
-        file1 <- sprintf("aligneddata/%d_ali_%d.txt", k, j);
-        file2 <- sprintf("aligneddata/%d_ali_%d.txt", j, k);
-        total_throughputs[i,1] <- TPpair(file1, file2, fps = fps, pca = pca, amc = amc, res = res, ftp = ftp, war = war, noise = noise)$tp;
-        total_throughputs[i,2] <- TPpair(file2, file1, fps = fps, pca = pca, amc = amc, res = res, ftp = ftp, war = war, noise = noise)$tp;
-        if (ftp) {
-            feature_throughputs[i,1] <- list(TPpair(file1, file2, fps = fps, pca = pca, amc = amc, res = res, ftp = ftp, war = war, noise = noise)$featTP);
-            feature_throughputs[i,2] <- list(TPpair(file2, file1, fps = fps, pca = pca, amc = amc, res = res, ftp = ftp, war = war, noise = noise)$featTP);
+        j = 2*i - 1;
+        k = j + 1;
+        file1 <- sprintf("aligneddata/%d_ali_%d.txt", j, k);
+        file2 <- sprintf("aligneddata/%d_ali_%d.txt", k, j);
+
+        results <- pair_throughput(file1, file2, fps = fps, pca = pca,
+            amc = amc, res = residuals, features = features, warnings = warnings,
+            noise = noise);
+        inverse_results <- pair_throughput(file2, file1, fps = fps, pca = pca,
+            amc = amc, res = residuals, features = features, warnings = warnings,
+            noise = noise);
+
+        total_throughputs[i,1] <- results$throughput;
+        total_throughputs[i,2] <- inverse_results$throughput;
+
+        if (features) {
+            feature_throughputs[i,1] <- list(results$feature_throughputs);
+            feature_throughputs[i,2] <- list(inverse_results$feature_throughputs);
         }
     }
-    if (ftp) {
-        return(list(total=total_throughputs, feature=feature_throughputs));
-    } else {
-        return(list(total=total_throughputs));
-    }
+    
+    if (features) {
+        return(list(total = total_throughputs, feature = feature_throughputs));
+    } else
+        return(list(total = total_throughputs));
 }
