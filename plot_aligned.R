0314d7a0ae8a734d01227958445f2aed9ad9b981
Laura Leppänen <laura.leppanen@iki.fi>
2012-03-05 14:42:11 +0200

    Fixed margins.

--- plot_aligned.R
+++ plot_aligned.R
@@ -5,7 +5,7 @@ source("data_handling.R")
 # directory.)
 plot_aligned <- function(seqnum1, seqnum2, outputfile = "test.pdf") {
     pdf(file = outputfile, height = 5, width = 7)
-    par(mfcol = c(2, 1), mar=c(5, 2, 2, 1))
+    par(mfcol = c(2, 1), mar = c(5, 4, 2, 1))
 
     draw_plot(seqnum1, seqnum2, sprintf("Sequence %d", seqnum1))
     draw_plot(seqnum2, seqnum1, sprintf("Sequence %d", seqnum2))
@@ -18,7 +18,6 @@ draw_plot <- function(seqnum1, seqnum2, mainlab) {
     sequence <- mean_normalize(load_aligned(seqnum1, seqnum2))
     colors = rainbow(ncol(sequence))
 
-    # Bug(?): for some reason ylab does not appear in the resulting pdf.
     plot(sequence[,1], type = "l", col = colors[1], ylim = c(min(sequence), max(sequence)),
         xlim = c(0, nrow(sequence)), xlab = "Frames", ylab = "Features",
         main = mainlab)
